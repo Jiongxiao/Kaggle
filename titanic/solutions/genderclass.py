b39e68f338e1b2cc341f917b83b68db9a7094fd8
Jiongxiao <tctjx@hotmail.com>
2016-03-12 00:35:52 +0800

    modify genderclassmodel

--- titanic/solutions/genderclass.py
+++ titanic/solutions/genderclass.py
@@ -2,7 +2,7 @@ import csv as csv
 import numpy as np
 
 # Open up the csv file in to a Python object
-train_file=open('test.csv', 'rb')
+train_file=open('train.csv', 'rb')
 train_file_object = csv.reader(train_file)
 header = train_file_object.next()  # The next() command just skips the
 data=[]
@@ -12,7 +12,7 @@ for row in train_file_object:
 data=np.array(data)
 
 fare_ceiling=40
-data[data[0:,9].astype(float)] >=fare_ceiling, 9]=fare_ceiling -1.0
+data[data[0:,9].astype(float) >= fare_ceiling, 9]=fare_ceiling -1.0
 fare_bracket_size=10
 number_of_price_brackets=fare_ceiling/fare_bracket_size
 
@@ -27,4 +27,8 @@ for i in xrange(number_of_classes):
         men=data[(data[0:,4]=='male') & (data[0:,2].astype(np.float)==i+1) & (data[0:,9].astype(np.float)> j*fare_bracket_size)\
         & (data[0:,9].astype(np.float)<(j+1)*fare_bracket_size),1]
 
-        survival_table[0,i,j]
+        survival_table[0,i,j]=np.mean(women.astype(np.float))
+        survival_table[1,i,j]=np.mean(men.astype(np.float))
+
+survival_table[survival_table!=survival_table]=0
+print survival_table
