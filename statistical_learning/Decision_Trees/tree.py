1e8ad22493b063bfe95ceb7e2d3b85233b1cd031
Jiongxiao <tctjx@hotmail.com>
2016-03-26 01:58:06 +0800

    modify tree.py

--- statistical_learning/Decision_Trees/tree.py
+++ statistical_learning/Decision_Trees/tree.py
@@ -1,12 +1,12 @@
 #coding: utf-8
 import calcuEntropy
 import numpy as np
-#输入带划分的数据集、划分数据集的特征、特征的返回值（只返回带该返回值的数据)
+
 
 #format of dataSet
 # array([ 1.(label),  1.,  1.,  0.,  3.,  0.,  0.])
 #      Survived  Pclass  SibSp  Parch  BinFare  Gender  EM
-
+#输入带划分的数据集、划分数据集的特征、特征的返回值（只返回带该返回值的数据)
 def splitDataSet(dataSet,axis,value):
     retDataSet=dataSet[(dataSet[0:,axis].astype(int)==value)]  #这里埋了一个雷，标签一定要转化成数字！！！
     retDataSet=np.delete(retDataSet,axis,axis=1)
@@ -29,7 +29,7 @@ def chooseBestFeat(dataSet):
             prob=len(subDataSet)/float(len(dataSet))
             newEntropy+=prob*calcuEntropy.calcShannonEnt(subDataSet,0)
         infoGain=baseEntropy-newEntropy
-        print infoGain
+        # print infoGain
         if (infoGain>bestInfoGain):
             bestFeat=i
             bestInfoGain=infoGain
@@ -50,3 +50,23 @@ def majorityC(classList):
     return sortedClassCount[0][0]
 # classList=[1,2,1,1,2,2,2,3,3,4,5,5,5]
 # print majorityC(classList)
+
+def createTree(dataSet,labels):
+    classList=[example[0] for example in dataSet]
+    if classList.count(classList[0])==1:
+        return classList[0]
+    if len(dataSet[0])==1:
+        return majorityC(classList)
+    bestFeat=chooseBestFeat(dataSet)
+    bestFeatLabel=labels[bestFeat]
+    myTree={bestFeatLabel:{}}
+    # del(labels[bestFeat])
+    labels=np.delete(labels,bestFeat)
+    featValues=[example[bestFeat] for example in dataSet]
+    uniqueVals=set(featValues)
+    for value in uniqueVals:
+        subLabels=labels[:]
+        myTree[bestFeatLabel][value]=createTree(splitDataSet(dataSet,bestFeat,value),subLabels)
+    return myTree
+
+train_tree=createTree(train_dataSet,labels)
